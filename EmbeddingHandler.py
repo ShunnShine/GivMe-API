180089cfd42a25f08c0c9d492222176806b21b74
Shunn Shine <jack.shunn@gmail.com>
2022-04-02 20:23:20 -0600

    first commit

---
+++ EmbeddingHandler.py
@@ -0,0 +1,115 @@
+import mxnet as mx
+import gluonnlp as nlp
+import numpy as np
+import os
+from sklearn.cluster import KMeans
+
+# Built with help from: 
+# Principal Component Analysis with NumPy
+# An article written by Wendy Navarrete
+# https://towardsdatascience.com/pca-with-numpy-58917c1d0391
+
+###########################
+# Load/Read Word Embedding
+###########################
+def loadDataVocab(embedType, preTrainFile):
+
+     fasttext_2M300d = nlp.embedding.create(embedType, source=preTrainFile)
+
+     # create vocabulary by using all the tokens
+     vocab = nlp.Vocab(nlp.data.Counter(fasttext_2M300d.idx_to_token))
+     vocab.set_embedding(fasttext_2M300d)
+     #len(vocab.idx_to_token)
+     count_tok = nlp.data.Counter(fasttext_2M300d.idx_to_token)
+     wordsVocab = [x[0] for x in count_tok.most_common()]
+
+     return vocab, wordsVocab[1:]
+
+def standardize_data(arr):
+         
+    '''
+    This function standardize an array, its substracts mean value, 
+    and then divide the standard deviation.
+    
+    param 1: array 
+    return: standardized array
+    '''    
+    rows, columns = arr.shape
+    
+    standardizedArray = np.zeros(shape=(rows, columns))
+    tempArray = np.zeros(rows)
+    
+    for column in range(columns):
+        
+        mean = np.mean(arr[:,column])
+        std = np.std(arr[:,column])
+        tempArray = np.empty(0)
+        
+        for element in arr[:,column]:
+            
+            tempArray = np.append(tempArray, ((element - mean) / std))
+ 
+        standardizedArray[:,column] = tempArray
+    
+    return standardizedArray
+
+
+def import_word_list(word_list, wordVocab, wordEmbedding):
+     word_list_embedding = []
+     cant_find = []
+     found = []
+     for word in word_list:
+          word = word.lower()
+          try:
+               i = wordVocab.index(word)
+               word_list_embedding.append(list(wordEmbedding[i]))
+               found.append(word)
+          except ValueError:
+               cant_find.append(word)
+     return found, cant_find, np.array(word_list_embedding, np.float32)
+
+def k_means_clus(wordEmbedding, num_of_clus):
+     return KMeans(n_clusters=num_of_clus).fit(wordEmbedding).labels_
+               
+
+def PCA(list_of_words):
+     embedType, preTrainFile = "glove", "glove.6B.300d"
+     vocab, all_wordsVocab = loadDataVocab(embedType, preTrainFile)
+
+     all_wordsVocab  = all_wordsVocab[:100000]
+     all_WordEmbedding = vocab.embedding[all_wordsVocab].asnumpy()
+
+     found_words_vocab, cant_find_words, wordlist_Embedding = import_word_list(list_of_words, all_wordsVocab, all_WordEmbedding)
+     # standardized it
+     wordlist_Embedding = standardize_data(wordlist_Embedding)
+
+     # compute eigen matrix
+     covariance_matrix = np.cov(wordlist_Embedding.T)
+     eigen_values, eigen_vectors = np.linalg.eig(covariance_matrix)
+
+
+     # # Calculating the explained variance on each of components
+     # variance_explained = []
+     # for i in eigen_values:
+     #      variance_explained.append((i/sum(eigen_values))*100)
+        
+     # cumulative_variance_explained = np.cumsum(variance_explained)
+
+     projection_matrix = (eigen_vectors.T[:][:3]).T
+     projected_words = wordlist_Embedding.dot(projection_matrix)
+
+     variance_explained = []
+     for i in eigen_values:
+          variance_explained.append((i/sum(eigen_values)).real*100)
+
+     #final step: get the projected coords
+     return found_words_vocab, cant_find_words, projected_words.real, sum(variance_explained[:3]), sum(variance_explained[:2])
+
+def Two_Means(word_list1, word_list2):
+     embedType, preTrainFile = "glove", "glove.6B.300d"
+     vocab, all_wordsVocab = loadDataVocab(embedType, preTrainFile)
+
+     embedding_Vocab  = all_wordsVocab[:100000]
+     embedding_vectors = vocab.embedding[all_wordsVocab].asnumpy()
+
+     found_words_vocab, cant_find_words, wordlist_Embedding = import_word_list(word_list1, all_wordsVocab, )
---
+++ app.py
@@ -0,0 +1,82 @@
+import re
+from flask import Flask, jsonify, request
+import EmbeddingHandler as eh
+import os
+app = Flask(__name__)
+
+
+@app.route("/<wordlist_name>")
+def json_response(wordlist_name):
+
+    labels_list, projected_embedding, eigen_values_sum3D, eigen_values_sum2D = eh.PCA(wordlist_name)
+    color_options = ["#00188f", "#009e49", "#00bcf2", "#ff8c00", "#e81123"]
+
+    clusters = []
+    for i in range(2, 6):
+        clusters.append(eh.k_means_clus(projected_embedding, i))
+
+    c_colors = [list(map(lambda x: color_options[x], cluster)) for cluster in clusters]
+
+    x_coords = list(projected_embedding[:,0])
+    y_coords = list(projected_embedding[:,1])
+    z_coords = list(projected_embedding[:,2]) # if dimensions_input == "3" else []
+    response = jsonify(
+        list_name = wordlist_name,
+        x_points = x_coords,
+        y_points = y_coords,
+        z_points = z_coords,
+        labels = labels_list,
+        colors = c_colors,
+        info_represented = [eigen_values_sum2D, eigen_values_sum3D]
+        )
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route("/initial_lists")
+def json_all_lists():
+    response =jsonify([name for name in os.listdir("./Lists")])
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route("/", methods=["OPTIONS"])
+def options():
+    return {'Allow' : 'PUT' }, 200, \
+    { 'Access-Control-Allow-Origin': '*', \
+        'Access-Control-Allow-Methods' : 'PUT,GET', \
+            'Access-Control-Allow-Headers' : '*' }
+
+@app.route("/", methods=['POST'])
+def Graphing_Request():
+    request_info = request.json
+    algorithm_type = request_info[0]
+    if algorithm_type == 'PCA':
+        labels_list, cant_find_words, projected_embedding, eigen_values_sum3D, eigen_values_sum2D = eh.PCA(request_info[1])
+        color_options = ["#00188f", "#009e49", "#00bcf2", "#ff8c00", "#e81123"]
+
+    clusters = []
+    for i in range(2, 6):
+        try:
+            clusters.append(eh.k_means_clus(projected_embedding, i))
+        except Exception:
+            break
+
+    c_colors = [list(map(lambda x: color_options[x], cluster)) for cluster in clusters]
+
+    x_coords = list(projected_embedding[:,0])
+    y_coords = list(projected_embedding[:,1])
+    z_coords = list(projected_embedding[:,2]) # if dimensions_input == "3" else []
+    response = jsonify(
+        x_points = x_coords,
+        y_points = y_coords,
+        z_points = z_coords,
+        labels = labels_list,
+        removed = cant_find_words,
+        colors = c_colors,
+        info_represented = [eigen_values_sum2D, eigen_values_sum3D]
+        )
+    if algorithm_type == '2M':
+        eh.Two_Means(request_info[1],request_info[2])
+
+    
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
